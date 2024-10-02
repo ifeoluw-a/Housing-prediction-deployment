62a131ec5f61106fe6857280a2308003887e5aca
ifeoluw-a <ifeoluwaadesanya19@gmail.com>
2024-10-02 16:03:51 +0100

    Web created

--- application/main.py
+++ application/main.py
@@ -1,1 +1,20 @@
 #streamlit web file
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+st.set_page_config(
+    layout= "wide", 
+    initial_sidebar_state= "expanded",
+    page_title= "Predict Price"
+)
+
+with st.sidebar:
+    selected= option_menu(
+        menu_title= None,
+        options=["Home", "Prediction", "Dashboard"],
+        icons=["house","",""],
+        menu_icon= "cast",
+        default_index= 0
+)
+
+    
